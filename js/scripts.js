7585313fa167d1265c8cbbf660bc08685e12faea
Joe Barnes and Marina Poltorak <joe@>
2019-04-02 10:55:51 -0700

    add logic to compare vowels to array

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(function(){
+  $('#formOne').submit(function(event){
+    event.preventDefault();
+
+    var sentence = $('#sentence1').val();
+    var sentArr = sentence.split('');
+    var vowels = ['a', 'e','i','o','u'];
+
+  for(var i = 0; i < sentArr.length; i++){
+    if(vowels.includes(sentArr[i])){
+      sentArr[i] = '-';
+      console.log(sentArr);
+    }
+    }
+
+  })
+})
